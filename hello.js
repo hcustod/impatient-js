ad15025a6f37142114852ab9692001ef2bbef040
hcustod <111096105+hcustod@users.noreply.github.com>
2022-10-21 22:51:47 -0400

    Recursion eg. freecodecamp

--- hello.js
+++ hello.js
@@ -1,2 +1,4 @@
 console.log('hello!');
 console.error("WRONG");
+
+ass
---
+++ object-literal.js
@@ -0,0 +1,12 @@
+// creating plain object via an object literal
+
+const obj = { 
+    first: 'Jane',
+    last: 'Doe',
+    getFullName() {
+        return this.first + ' ' + this.last
+    },
+};
+
+
+
