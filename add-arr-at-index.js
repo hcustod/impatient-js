3991b3e61e61ff2b1d74c93a06a3d6e2b398246e
hcustod <111096105+hcustod@users.noreply.github.com>
2022-10-24 18:05:44 -0400

    adding array elements at given index - FCC lesson

---
+++ add-arr-at-index.js
@@ -0,0 +1,9 @@
+// from fcc lesson 
+
+function frankenSplice(arr1, arr2, n) {
+    new_arr = [...arr2];
+    new_arr.splice(n, 0, ...arr1);
+    return new_arr;
+  }
+  
+  console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
