0bce5e18f0725c5722f52b684d54a367a533fd2e
hcustod <111096105+hcustod@users.noreply.github.com>
2022-11-27 23:18:11 -0500

    more algorithms

---
+++ algorithm-exs/symmetric-difference.js
@@ -0,0 +1,35 @@
+/// fcc imperative solution
+
+function diffArray(arr1, arr2) {
+    const newArr = [];
+  
+    function onlyInFirst(first, second) {
+      for (let i = 0; i < first.length; i++) {
+        if (second.indexOf(first[i]) === -1) {
+          newArr.push(first[i]);
+        }
+      }
+    }
+  
+    onlyInFirst(arr1, arr2);
+    onlyInFirst(arr2, arr1);
+  
+    return newArr;
+  }
+  
+  diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+
+  /// fcc declarative solution
+/*
+
+
+  function diffArray(arr1, arr2) {
+    return arr1
+      .concat(arr2)
+      .filter(item => !arr1.includes(item) || !arr2.includes(item));
+  }
+  
+  diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+  */
