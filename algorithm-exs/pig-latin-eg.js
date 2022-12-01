ff366d8e909a681db73f897941a6c984488274a2
hcustod <111096105+hcustod@users.noreply.github.com>
2022-11-30 23:52:41 -0500

    More fcc algorithms

--- algorithm-exs/pig-latin-eg.js
+++ algorithm-exs/pig-latin-eg.js
@@ -13,3 +13,16 @@ function translatePigLatin(str) {
   }
   
   translatePigLatin("consonant");
+
+
+//   simpler solution using declarative methods 
+
+function translatePigLatin2(str) {
+  return str
+    .replace(/^[aeiou]\w*/, "$&way")
+    .replace(/(^[^aeiou]+)(\w*)/, "$2$1ay");
+}
+
+// test here
+translatePigLatin2("consonant");
+
